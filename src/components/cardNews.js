eb178b2df6226001201492f52dfeb811712dcb38
Filipe Zeferino <filipezef@gmail.com>
2022-12-15 08:01:54 -0300

    2 components created for learning/training purposes (cadNews.js and dinamicTitle.js), the creating was in the sequence of reference in this comment, the second component is slightly more complex where the component main parts are defined and commented

--- src/components/cardNews.js
+++ src/components/cardNews.js
@@ -1,17 +1,17 @@
 class CardNews extends HTMLElement {
   constructor() {
-    super() // this will invoke the constructor function from the class which this class is inheriting from
+    super() // this will invoke the constructor function from the class which this class is inheriting from (in this case HTMLElement)
 
     const shadow = this.attachShadow({ mode: 'open' }) // this line means that you are attaching a shadow to your class ("this" refers to the class we are in), and this shadow will be open to other js files to be manipulated. If we wanted only the class CardNews code to be abble to manipulate the shadow, we would define the mode as "closed".
-    shadow.innerHTML = '<h1>Hello Filipe, this is your first component</h1>' // this line means that we will add to the constant shadow some HTML code, and this code is the tag h1 above with its content.
+    shadow.innerHTML = '<h1>Hello Filipe, this is your first component</h1>' // this line means that we will add to the constant shadow some HTML code, and this code is the tag h1 above with its content (prop).
   }
 }
 
-customElements.define('card-news', CardNews) // this line means that the custom elemet we are creating will be called "cardNews" (we call this name selector, so the selector cardNews will invoke this custom element) and this element is instanced from the class "CardNews".
-// IMPORTANT: when defining the selector name, do not use camelCase because the browser once it parses the HTML page it will turn the selector name into lower case
+customElements.define('card-news', CardNews) // this line means that the component we are creating will be called "card-news" (we call this name selector, so the selector card-news will invoke this component) and this component is instanced from the class "CardNews". A component is formed by a collection of elements (p, h1, a tags and so on, each HTML tag is a component element). The component can have its own styles and properties (usually called props). Props are the content for each element (as tabs images have their own src), properties can be dymanic, so you can use a component in a dynamic manner.
+// IMPORTANT: when defining the selector name, do not use camelCase because the browser once it parses the HTML page it will turn the selector name into lower case. By default the selector is written with hyphen.
 
-// this selector cardNews will be linked to the desired HTML document through the HTML tag <script> as it follows:
+// this selector card-news will be linked to the desired HTML document through the HTML tag <script> as it follows:
 // <script src="./src/components/cardNews.js" defer></script>
 // the defer boolean attribute tells to the browser that the script must be executed only after the HTML file is completely parsed.
 // IMPORNTANT: check that when inspecting the HTML page you can notice that within the document element (DOM) you have a shadow DOM which is indicated by #shadow-root, which is your shadow DOM.
-// you can add styles and properties to the custom element, so instead of always been parsed with the same content, you can vary this content and style according to the context of which the component is invoked.
+// you can add styles and properties to the copmonent, so instead of always been parsed with the same content, you can vary this content and style according to the context of which the component is invoked.
---
+++ src/components/dinamicTitle.js
@@ -0,0 +1,27 @@
+class DinamicTitle extends HTMLElement {
+  constructor() {
+    super()
+
+    const shadow = this.attachShadow({ mode: 'open' })
+
+    //component base
+    const componentRoot = document.createElement('h1') // when you invoke the method createElement from DOM it gives you access to every HTML5 tag.
+    componentRoot.textContent = 'Filipe Zeferino' // So basecally you created a H1 HTML tag with the content "Filipe Zeferino" in it
+
+    //component style
+    const style = document.createElement('style') // here you are creating an HTML style tag (the one you can you on the HTML head tag), refer to it on DEV notes. We will add its content using template string, so that we can apply dynamic styles to it later on using JS.
+    // IMPORTANT: all styles you create for a component are scoped, so they will only work for that component they belong to. Remember that this component is created in a shadow which is kind of independent of DOM.
+    style.textContent = `
+    h1 {
+      color: red;
+      padding: 2rem;
+    }
+    `
+
+    //send component to shadow
+    shadow.appendChild(componentRoot)
+    shadow.appendChild(style)
+  }
+}
+
+customElements.define('dinamic-title', DinamicTitle)
